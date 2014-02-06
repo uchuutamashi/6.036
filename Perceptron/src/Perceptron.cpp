bc75ffc2f4fc77f513480f4f17b411c60b956c9b
Uchuu Tamashi <ytc@mit.edu>
2014-02-06 18:54:57 -0500

    bug fix

--- Perceptron/src/Perceptron.cpp
+++ Perceptron/src/Perceptron.cpp
@@ -24,21 +24,28 @@ bool Perceptron::Train(const std::vector<double>& input, const int& output) { //
 
 void Perceptron::Train( std::ifstream& data , bool verbose /* = false */ ) {
   size_t N;  // number of points in data set
-  std::vector<double> x {0,_insize};
-  int y;
+  std::vector<std::vector<double>> x;
+  std::vector<int> y;
   bool cont = false; // whether training should continue
   int count = 0;
   
   data >> N; // read number of points
+  for( size_t i=0; i < N; ++i ) {
+	x.push_back(std::vector<double>{0.0,_insize});
+	for( size_t j=0; j < _insize; ++j ) {
+	  data >> x[i][j];
+	  if(verbose) std::cout << x[i][j] << " ";
+	}
+	y.push_back(0);
+	data >> y[i];
+    if(verbose) std::cout << y[i] << std::endl;	
+  }
+  
   
   do{
 	cont = false; // reset flag
-    for( size_t i=0; i < N; ++i ) {
-	    for( size_t j=0; j < _insize; ++j ) {
-		    data >> x[j];
-	    }
-	    data >> y;
-		if( Train(x,y) ) { // if a mistake has been encountered
+    for( size_t i=0; i < N; ++i ) {	   
+		if( Train(x[i],y[i]) ) { // if a mistake has been encountered
 	      cont = true;
 		  if( verbose ) { // print a message
 			  std::cout << "Mistake encountered at point " << i << std::endl;
